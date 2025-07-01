1354f628380f3bd46dcbefedb09de38a92c1e56c
Prathamsharma45 <ps155567@gmail.com>
2025-07-01 23:51:15 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,6 @@
+a=1
+print(type(a))
+b=3.14
+print(type(b))
+c="prathamsharma"
+print(type(c))
---
+++ inputfun.py
@@ -0,0 +1,8 @@
+int=a=input("enter your number")
+int=b=input("enter your number")
+
+print("number 1:is",(a))
+print("number 2:is",(b))
+print("sum of two no",a+b)
+print(type(a))
+print(type(b))
